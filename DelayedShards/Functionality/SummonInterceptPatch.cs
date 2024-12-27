73bbc75fe18687fb615b6fa4cfef7d112a1ffd7a
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-12-26 22:58:13 -0800

    Added new incrimenting code.

---
+++ DelayedShards/Functionality/SummonInterceptPatch.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+
+namespace DelayedShards.Functionality
+{
+    //Block origional consumption code from running and incriment counter.
+    [HarmonyPatch(typeof(CarryableSummonConsumptionEffect), "OnConsume")]
+    internal class SummonInterceptPatch
+    {
+        static bool Prefix(CarryableSummonConsumptionEffect __instance)
+        {
+            if (!Configs.enableQueue.Value) return true;
+
+            if (__instance.carryable.assetGuid == Helper.MinefieldGUID)
+            {
+                Helper.MinefieldsAvailable++;
+                ShardMessageHandler.SendShardCount();
+                return false;
+            }
+            else if (__instance.carryable.assetGuid == Helper.EscortGUID)
+            {
+                Helper.EscortsAvailable++;
+                ShardMessageHandler.SendShardCount();
+                return false;
+            }
+            return true;
+        }
+    }
+}
