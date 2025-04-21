9ad1f9f2f30f737e58c568af1891e9acdcd934ce
Nagord <46509577+Nagord@users.noreply.github.com>
2025-04-21 15:28:33 -0700

    Updated documentation

--- DelayedShards/MyPluginInfo.cs
+++ DelayedShards/MyPluginInfo.cs
@@ -7,11 +7,11 @@ namespace DelayedShards
         public const string PLUGIN_GUID = $"{PLUGIN_ORIGINAL_AUTHOR}.{PLUGIN_NAME}";
         public const string PLUGIN_NAME = "DelayedShards";
         public const string USERS_PLUGIN_NAME = "Delayed Shards";
-        public const string PLUGIN_VERSION = "0.1.1";
+        public const string PLUGIN_VERSION = "0.1.2";
         public const string PLUGIN_DESCRIPTION = "Stores functional shards when inserted into the astral map, which may be activated on demand.";
         public const string PLUGIN_ORIGINAL_AUTHOR = "18107";
         public const string PLUGIN_AUTHORS = "18107, Dragon";
-        public const string PLUGIN_THUNDERSTORE_ID = "VoidCrewModdingTeam/Delayed_Shards";
+        public const string PLUGIN_THUNDERSTORE_ID = "NihilityShift/Delayed_Shards";
     }
 }
 #pragma warning restore CS1591
--- DelayedShards/ReleaseFiles/CHANGELOG.md
+++ DelayedShards/ReleaseFiles/CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.1.2
+- Updated for Void Crew 1.1.0
+
 ## 0.1.1
 - Fixed audio calls
 
